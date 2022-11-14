e888673567b1cfee4e6e19898f87be22986629d6
Lee-na-yun <happyin3333@nate.com>
2022-11-14 23:54:17 +0900

    가위바위보게임 바위아이콘 추가

---
+++ src/Dice.js
@@ -0,0 +1,11 @@
+import diceBlue01 from "./assets/dice-blue-1.svg";
+
+function Dice() {
+  return (
+    <div>
+      <img src={diceBlue01} alt="주사위" />
+    </div>
+  );
+}
+
+export default Dice;
---
+++ src/HandIcon.js
@@ -0,0 +1,11 @@
+import rock from "./assets/rock.svg";
+
+function HandIcon() {
+  return (
+    <>
+      <img src={rock} alt="바위" />
+    </>
+  );
+}
+
+export default HandIcon;
