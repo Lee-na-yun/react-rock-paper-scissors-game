4357d77fc840694efd081e417bcb5ac7b8843960
Lee-na-yun <happyin3333@nate.com>
2022-11-15 22:46:13 +0900

    주사위 랜덤으로 던지기 기능 완료

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import HandIcon from "./HandIcon";
 import Dice from "./Dice";
 import HandButton from "./HandButton";
 import Button from "./Button";
+import { useState } from "react";
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
-  const handleClick = (value) => console.log(value);
-  const handleClearClick = () => console.log("처음부터");
+  //const handleClick = (value) => console.log(value);
+  //const handleClearClick = () => console.log("처음부터");
+
+  const [num, setNum] = useState(1);
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  };
+
   return (
     <>
-      <Button onClick={handleClearClick}>처음부터</Button>
+      {/* <Button onClick={handleClearClick}>처음부터</Button>
       <HandButton value="rock" onClick={handleClick} />
       <HandButton value="scissor" onClick={handleClick} />
-      <HandButton value="paper" onClick={handleClick} />
+      <HandButton value="paper" onClick={handleClick} /> */}
 
-      {/* <div>
-        <Button>던지기</Button>
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
         <Button>처음부터</Button>
       </div>
-      <Dice color="red" num={2}></Dice> */}
+      <Dice color="red" num={num} />
     </>
   );
 }
