3c7724e437386dbb0a688bd48e69f81ae4d367f2
Lee-na-yun <happyin3333@nate.com>
2022-11-15 22:23:53 +0900

    props children 주사위  예제

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import HandIcon from "./HandIcon";
 import Dice from "./Dice";
 import HandButton from "./HandButton";
+import Button from "./Button";
 
 function App() {
-  const handleClick = (value) => console.log(value);
+  //const handleClick = (value) => console.log(value);
   return (
     <>
-      <HandButton value="rock" onClick={handleClick} />
+      {/* <HandButton value="rock" onClick={handleClick} />
       <HandButton value="scissor" onClick={handleClick} />
-      <HandButton value="paper" onClick={handleClick} />
+      <HandButton value="paper" onClick={handleClick} /> */}
+      <div>
+        <Button>던지기</Button>
+        <Button>처음부터</Button>
+      </div>
+      <Dice color="red" num={2}></Dice>
     </>
   );
 }
