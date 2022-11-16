9f6986b7ad9a625b03b2f11e93dccbcbbafd96b0
Lee-na-yun <happyin3333@nate.com>
2022-11-16 23:25:57 +0900

    클래스네임으로 스타일 적용 수정 완료

--- src/Button.js
+++ src/Button.js
@@ -1,30 +1,9 @@
-const baseButtonstyle = {
-  //속성: "값",
-  padding: "14px 27px",
-  outline: "none",
-  cursor: "pointer",
-  borderRadius: "30px",
-  fontSize: "17px",
-};
+import './Button.css';
 
-const blueButtonStyle = {
-  ...baseButtonstyle,
-  backgroundColor: "rgba(0,89,255,0.2)",
-  border: "solid 1px #7090ff",
-  color: "#7090ff",
-};
-
-const redButtonStyle = {
-  ...baseButtonstyle,
-  backgroundColor: "rgba(255,78,78,0.2)",
-  border: "solid 1px #ff4664",
-  color: "#ff466f",
-};
-
-function Button({ children, onClick, color}) {
-  const style = color === 'red'? redButtonStyle : blueButtonStyle;
+function Button({ children, onClick, color = 'blue'}) {
+  const classNames = `Button ${color}`; // 클래스네임값이 바뀌게 하기
   return (
-    <button style={style} onClick={onClick}>
+    <button className={classNames} onClick={onClick}>
       {children}
     </button>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"; // css 임포트는 파일명을 바로 적어주기
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
