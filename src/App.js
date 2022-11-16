abbba10f0e6c7eceab2f20b55dacfe25f3ff4575
Lee-na-yun <happyin3333@nate.com>
2022-11-16 22:44:32 +0900

    코드 간소화하기 완료

--- src/App.js
+++ src/App.js
@@ -8,32 +8,19 @@ function random(n) {
 }
 
 function App() {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
-  const [othernum, setOtherNum] = useState(1);
-  const [othersum, setOtherSum] = useState(0);
-  const [othergameHistory, setOtherGameHistory] = useState([]);
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
 
   const handleRollClick = () => {
-    const nextNum = random(6);
+    const nextMyNum = random(6);
     const nextOtherNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    //gameHistory.push(nextNum); //gameHistory가 배열이니까 push메소드로 nextNum 추가
-    setGameHistory([...gameHistory, nextNum]); // set함수로 새 값이 추가된 gameHistory state 전달하기
-    setOtherNum(nextNum);
-    setOtherSum(sum + nextNum);
-    setOtherGameHistory([...gameHistory, nextNum]);
+    setMyHistory([...myHistory, nextMyNum]); // set함수로 새 값이 추가된 gameHistory state 전달하기
+    setOtherHistory([...otherHistory, nextOtherNum]);
   };
 
   const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-    setOtherNum(1);
-    setOtherSum(0);
-    setOtherGameHistory([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   };
 
   return (
@@ -43,8 +30,8 @@ function App() {
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
-        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
-        <Board name="상대" color="red" num={othernum} sum={othersum} gameHistory={othergameHistory} />
+        <Board name="나" color="blue" gameHistory={myHistory} />
+        <Board name="상대" color="red" gameHistory={otherHistory} />
       </div>
     </div>
   );
--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,10 @@
 import Dice from "./Dice";
 
-
-function Board({name, color, num, sum, gameHistory}) {
+function Board({ name, color, gameHistory }) {
+  const num = gameHistory[gameHistory.length - 1] || 1;
+  const sum = gameHistory.reduce((a, b) => a + b, 0);
+  // reduce() : 배열의 각 요소에 대해 주어진 reducer 함수를 실행하고, 하나의 결과값을 반환함
+  // array.reduce((acc,cur,index,element) => {return 결과}, 초기값);
   return (
     <>
       <div>
@@ -10,7 +13,7 @@ function Board({name, color, num, sum, gameHistory}) {
         <h2>총점</h2>
         <p>{sum}</p>
         <p>기록</p>
-        <p>{gameHistory.join(',')}</p> 
+        <p>{gameHistory.join(",")}</p>
       </div>
     </>
   );
