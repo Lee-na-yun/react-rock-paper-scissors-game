ba569e28eae1cb645bb938098d0ad4b6a27b9117
Lee-na-yun <happyin3333@nate.com>
2022-11-16 22:15:53 +0900

    주사위던지기 게임 컴포넌트화 하기

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
 import "./App.css";
-import HandIcon from "./HandIcon";
-import Dice from "./Dice";
-import HandButton from "./HandButton";
-import Button from "./Button";
-import { useState } from "react";
-import { generateRandomHand, compareHand } from "./utils";
-
-const INIT_VALUE = "rock";
-
-function getResult(me, other) {
-  const comparison = compareHand(me, other);
-  if (comparison > 0) return "승리";
-  if (comparison < 0) return "패배";
-  return "무승부";
-}
+import Board from "./Board";
 
 function App() {
-  const [hand, setHand] = useState(INIT_VALUE);
-  const [otherHand, setOtherHand] = useState(INIT_VALUE);
-  const [gameHistory, setGameHistory] = useState([]);
-
-  const handleButtonClick = (nextHand) => {
-    const nextOtherHand = generateRandomHand();
-    const nextHistoryItem = getResult(nextHand, nextOtherHand);
-    setHand(nextHand);
-    setOtherHand(nextOtherHand);
-    setGameHistory([...gameHistory, nextHistoryItem]);
-  };
-
-  const handleClearClick = () => {
-    setHand(INIT_VALUE);
-    setOtherHand(INIT_VALUE);
-    setGameHistory([]);
-  };
-
+  
   return (
     <div>
-      <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)}</p>
-      <div>
-        <HandIcon value={hand} />
-        VS
-        <HandIcon value={otherHand} />
-      </div>
-      <p>승부 기록: {gameHistory.join(", ")}</p>
-      <div>
-        <HandButton value="rock" onClick={handleButtonClick} />
-        <HandButton value="scissor" onClick={handleButtonClick} />
-        <HandButton value="paper" onClick={handleButtonClick} />
-      </div>
+      <Board name="나" color="blue"/>
+      <Board name="상대" color="red"/>
     </div>
   );
 }
